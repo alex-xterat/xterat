3e6f116c91e87a076c850e17faa3c7d92f0c48e8
hashir-zylotech <muhammad.hashir@zylotech.com>
2019-04-03 01:39:26 +0530

    Added Logo and Branding

---
+++ src/components/others/header/logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppLink from '../link/link'
+
+const HeaderLogo = () => (
+  <div className="logo">
+    <AppLink url="/">
+      <img src="/images/xterat.png" alt="Xterat" style="width: 81px; top: -12px; right:75px;">
+    </AppLink>
+  </div>
+);
+
+export default HeaderLogo;
