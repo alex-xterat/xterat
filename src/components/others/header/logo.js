f9a93983b3b66754f601b6d031f6aad1e46d9758
hashir-zylotech <muhammad.hashir@zylotech.com>
2019-04-03 12:50:43 +0530

    Fixed prod bugs

--- src/components/others/header/logo.js
+++ src/components/others/header/logo.js
@@ -4,7 +4,7 @@ import AppLink from '../link/link'
 const HeaderLogo = () => (
   <div className="logo">
     <AppLink url="/">
-      <img src="/images/xterat.png" alt="Xterat" style="width: 81px; top: -12px; right:75px;">
+      <img src="/images/xterat.png" alt="Xterat" style="width: 81px; top: -12px; right:75px;"/>
     </AppLink>
   </div>
 );
